197a109b2560db42420421da905d942c7a24f2a1
Joshua Shirey <jshirey5555@gmail.com>
2025-07-19 18:09:05 -0400

    8/8 tests

--- app.js
+++ app.js
@@ -4,21 +4,36 @@ import express from "express"
 const app = express()
 
 app.route("/").get((req, res) => {
-    res.send("Hello World!")
+    res.send("Hello employees!")
 })
 
 app.get("/employees", (req, res) => {
-    res.send(employees)
+    res.status(200).send(employees)
 })
 
-app.get("/employees/:name", (req, res) => {
-    const { name } = req.params
+app.get("/employees/random", (req, res) => {
+    // const { random } = req.params
+    const randNum = Math.floor(Math.random() *employees.length)
+    const randEmp = employees[randNum]
 
-if (!employees.includes(name)) {
-    return res.status(404).send("name not found")
-    }   
+    if (!randEmp){
+        return res.status(404).send(`Random employee is not found`)
+    }
 
-    res.send(true)
+    res.status(200).json(randEmp)
 })
 
+app.get("/employees/:id", (req, res) => {
+    const { id } = req.params
+    const employee = employees.find(emp => emp.id === parseInt(id));
+
+    if (!employee) {
+        return res.status(404).send(`Employee ${id} not found`)
+    }
+
+    res.status(200).json(employee);
+});
+
+
+     
 export default app
